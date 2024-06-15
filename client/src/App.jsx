3d6f6efadf35216d79da77b117ad7b9f73560658
P SHREEKAR SHENOY <shreekarshenoy@gmail.com>
2024-06-15 22:09:32 +0530

    resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import FeatureSection from "./components/FeatureSection";
 import LandingPage from "./pages/LandingPage";
-import TextSummarizer from "./pages/TextSummarizer"; // This line was added from incoming branch
-import ContactUs from "./pages/ContactUs"; // This line was added from your branch
+import TextSummarizer from "./pages/TextSummarizer"; 
+import ContactUs from "./pages/ContactUs"; 
 
 function App() {
   return (
     <div>
       <LandingPage />
-      <ContactUs />  {/* This line was added from your branch */}
-      <TextSummarizer />  {/* This line was added from incoming branch */}
+      <ContactUs />  
+      <TextSummarizer /> 
     </div>
   );
 }
