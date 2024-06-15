0d39b6509e2105cab4ff2bcb07b4b33234f3d7e2
Praharsh-T <ppraharsht@gmail.com>
2024-06-16 00:24:54 +0530

    changed designs

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -1,61 +1,74 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function ContactUs() {
   return (
-    <div>
-      <section className='bg-white dark:bg-gray-900'>
-        <div className='py-8 lg:py-16 px-4 mx-auto max-w-screen-md'>
-          <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white'>
+    <div className='flex justify-center items-center min-h-screen bg-black text-white font-mono'>
+      <section className='bg-black text-white p-10 rounded-lg border-4 border-gray-100 max-w-lg w-full mt-7 shadow-neon mb-9'>
+        <div className='px-4 mx-auto'>
+          <div className='flex justify-between items-center mb-4'>
+            <div className='flex space-x-2 text-red-500'>
+              <div className='w-3 h-3 rounded-full bg-red-500'></div>
+              <div className='w-3 h-3 rounded-full bg-yellow-500'></div>
+              <div className='w-3 h-3 rounded-full bg-green-500'></div>
+            </div>
+            <Link
+              to='/'
+              className='text-sm font-mono font-semibold text-gray-100 border-y-2 '>
+              Home
+            </Link>
+          </div>
+          <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-center font-mono text-white'>
             Contact Us
           </h2>
-          <p className='mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl'>
+          <p className='mb-8 font-mono font-light text-center text-gray-400 sm:text-xl'>
             Got a technical issue? Want to send feedback about a beta feature?
             Need details about our Business plan? Let us know.
           </p>
           <form action='#' className='space-y-8'>
             <div>
               <label
-                for='email'
-                className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
+                htmlFor='email'
+                className='block mb-2 font-mono text-sm font-medium text-gray-300'>
                 Your email
               </label>
               <input
                 type='email'
                 id='email'
-                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                className='shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-green-400 focus:border-green-400 block w-full p-2.5'
                 placeholder='name@flowbite.com'
                 required
               />
             </div>
             <div>
               <label
-                for='subject'
-                className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'>
+                htmlFor='subject'
+                className='block mb-2 font-mono text-sm font-medium text-gray-300'>
                 Subject
               </label>
               <input
                 type='text'
                 id='subject'
-                className='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                className='block p-3 w-full text-sm text-white bg-gray-800 rounded-lg border border-gray-600 shadow-sm focus:ring-green-400 focus:border-green-400'
                 placeholder='Let us know how we can help you'
                 required
               />
             </div>
             <div className='sm:col-span-2'>
               <label
-                for='message'
-                className='block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400'>
+                htmlFor='message'
+                className='block mb-2 font-mono text-sm font-medium text-gray-300'>
                 Your message
               </label>
               <textarea
                 id='message'
                 rows='6'
-                className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+                className='block p-2.5 w-full text-sm text-white bg-gray-800 rounded-lg shadow-sm border border-gray-600 focus:ring-green-400 focus:border-green-400'
                 placeholder='Leave a comment...'></textarea>
             </div>
             <button
               type='submit'
-              className='py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>
+              className='py-3 px-5 font-mono text-sm font-bold text-center text-white rounded-lg bg-green-900 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300'>
               Send message
             </button>
           </form>
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import img1 from "./img1.svg";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import TextSummarizer from "./TextSummarizer";
 
 function LandingPage() {
   return (
-    <div className='min-h-screen min-w-full bg-gradient-to-r from-slate-900 to-emerald-400 flex flex-col justify-center p-10'>
+    <div className='min-h-screen min-w-full bg-gradient-to-tr from-gray-900 via-green-400 to-gray-900 flex flex-col justify-center p-10'>
       <div className='relative w-full max-w-full lg:max-w-6xl xl:max-w-screen-2xl mx-auto'>
-        <div className='absolute inset-0 -mr-3.5 bg-gradient-to-r from-emerald-300 to-slate-900 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:rotate-3 sm:rounded-3xl'></div>
-        <div className='relative bg-gradient-to-r from-emerald-700 to-slate-900 shadow-lg sm:rounded-3xl'>
+        <div className='absolute inset-0 -mr-3.5 bg-gradient-to-r from-slate-900 to-slate-900  transform -skew-y-6 sm:skew-y-0 sm:rotate-3 sm:rounded-3xl shadow-neon'></div>
+        <div className='relative bg-gradient-to-br from-gray-900 via-green-400 to-gray-900  sm:rounded-3xl shadow-neon'>
           <div className='flex items-center justify-start pt-6 pl-6'>
             <span className='w-3 h-3 bg-red-400 rounded-full mr-2'></span>
             <span className='w-3 h-3 bg-yellow-400 rounded-full mr-2'></span>
@@ -17,9 +17,9 @@ function LandingPage() {
           <div className='px-12 py-8'>
             <div className='flex items-center justify-between'>
               <div className='flex items-center justify-center'>
-                <div className='flex items-center justify-center text-3xl font-bold text-white'>
+                <div className='flex items-center justify-center text-3xl font-bold font-mono text-white'>
                   <svg
-                    className='w-10 h-10 mr-1 text-blue-700'
+                    className='w-10 h-10 mr-1 text-white'
                     fill='none'
                     stroke='currentColor'
                     viewBox='0 0 24 24'
@@ -40,7 +40,7 @@ function LandingPage() {
                 <div className='hidden lg:flex items-center justify-center antialiased lg:ml-20 pt-1'>
                   <a
                     href='#'
-                    className='flex items-center justify-center mr-10 text-base text-white text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out'>
+                    className='flex items-center justify-center mr-10 text-base text-white font-mono text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out'>
                     Product
                     <svg
                       className='w-3.5 h-4 ml-2'
@@ -55,9 +55,9 @@ function LandingPage() {
                         d='M19 9l-7 7-7-7'></path>
                     </svg>
                   </a>
-                  <a
-                    href='#'
-                    className='flex items-center justify-center mr-10 text-base text-white text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out'>
+                  <Link
+                    to={"/ContactUs"}
+                    className='flex items-center justify-center mr-10 text-base font-mono text-white text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out'>
                     Contact Us
                     <svg
                       className='w-3.5 h-4 ml-2'
@@ -71,10 +71,10 @@ function LandingPage() {
                         strokeWidth='3'
                         d='M19 9l-7 7-7-7'></path>
                     </svg>
-                  </a>
+                  </Link>
                   <a
                     href='#'
-                    className='flex items-center justify-center mr-10 text-base text-white text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out'>
+                    className='flex items-center justify-center mr-10 text-base font-mono text-white text-opacity-90 font-medium tracking-tight hover:text-cool-gray-600 transition duration-150 ease-in-out'>
                     About
                   </a>
                 </div>
@@ -82,10 +82,10 @@ function LandingPage() {
               <div className='hidden md:flex items-center justify-center'>
                 <a
                   href='#'
-                  className='mr-5 text-lg font-medium text-white hover:text-cool-gray-700 transition duration-150 ease-in-out'>
+                  className='mr-5 text-lg font-medium text-white font-mono hover:text-cool-gray-700 transition duration-150 ease-in-out'>
                   Login
                 </a>
-                <button className='px-6 py-3 rounded-3xl font-medium bg-gradient-to-b from-gray-900 to-black text-white outline-none focus:outline-none hover:shadow-md hover:from-true-gray-900 transition duration-200 ease-in-out'>
+                <button className='px-6 py-3 rounded-3xl font-medium font-mono bg-gradient-to-b from-gray-900 to-black text-white outline-none focus:outline-none hover:shadow-md hover:from-true-gray-900 transition duration-200 ease-in-out'>
                   Sign Up
                 </button>
               </div>
@@ -93,13 +93,13 @@ function LandingPage() {
 
             <div className='flex flex-col lg:flex-row lg:items-center lg:justify-between mt-20 lg:mt-10 lg:ml-16'>
               <div className='text-left lg:w-1/2'>
-                <div className='text-6xl font-semibold text-green-100 leading-none'>
+                <div className='text-6xl font-semibold font-mono text-white leading-none'>
                   Effortless Text Summarization
                 </div>
-                <div className='mt-6 text-xl font-light text-green-200 antialiased'>
+                <div className='mt-6 text-xl font-bold font-mono text-white antialiased'>
                   Instantly condense lengthy documents into concise summaries.
                 </div>
-                <button className='mt-6 px-8 py-4 rounded-full font-normal tracking-wide bg-gradient-to-b from-green-200 to-green-500 text-white outline-none focus:outline-none hover:shadow-lg hover:from-green-700 transition duration-200 ease-in-out'>
+                <button className='mt-6 px-8 py-4 rounded-full font-mono font-semibold tracking-wide bg-gradient-to-b from-green-200 to-green-500 text-white outline-none focus:outline-none hover:shadow-lg hover:from-green-700 transition duration-200 ease-in-out'>
                   Get Started
                 </button>
               </div>
