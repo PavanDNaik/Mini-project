a30edc29c00da426086f6be6c2b6e7ba2303d8df
P SHREEKAR SHENOY <shreekarshenoy@gmail.com>
2024-06-15 21:52:41 +0530

    contact us

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import FeatureSection from "./components/FeatureSection";
 import LandingPage from "./pages/LandingPage";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
     <>
       <div>
         <LandingPage />
+        <ContactUs />
       </div>
     </>
   );
