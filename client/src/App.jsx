07e895279fe09c44c7cf8b881fbf0a7d09189a5d
P SHREEKAR SHENOY <shreekarshenoy@gmail.com>
2024-06-17 18:46:56 +0530

    testimonials

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Footer from "./components/Footer";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path='login' element={<LoginPage />} />
         <Route path='register' element={<RegisterPage />} />
       </Routes>
+      <Testimonials/>
       <Footer />
     </BrowserRouter>
     
